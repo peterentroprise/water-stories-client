b75d8f74e603e431e4a5e9084abfd8862a8d9fe4
Peter Arnold <peter@entroprise.com>
2021-04-06 15:30:55 -0400

    clean

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,8 +1,6 @@
 import { extendTheme } from "@chakra-ui/react";
 import { createBreakpoints } from "@chakra-ui/theme-tools";
 
-const fonts = { mono: `'Menlo', monospace` };
-
 const breakpoints = createBreakpoints({
   sm: "40em",
   md: "52em",
