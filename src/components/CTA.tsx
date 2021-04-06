bff9bbf238f3bd5109fcaa9e874d5b14e7f9542c
Peter Arnold <peter@entroprise.com>
2021-04-06 16:27:29 -0400

    colors

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -9,16 +9,16 @@ export const CTA = () => (
     bottom="0"
     width="100%"
     maxWidth="48rem"
-    py={2}
+    py={4}
   >
-    <ChakraLink isExternal href="/" flexGrow={1} mx={2}>
-      <Button width="100%" variant="outline" colorScheme="green">
+    <ChakraLink isExternal href="/" flexGrow={1} ml={4} mr={2}>
+      <Button width="100%" variant="solid" colorScheme="gray">
         Create Account
       </Button>
     </ChakraLink>
 
-    <ChakraLink isExternal href="/" flexGrow={3} mx={2}>
-      <Button width="100%" variant="solid" colorScheme="green">
+    <ChakraLink isExternal href="/" flexGrow={3} ml={2} mr={4}>
+      <Button width="100%" variant="solid" colorScheme="purple">
         Sign In
       </Button>
     </ChakraLink>
--- src/theme.tsx
+++ src/theme.tsx
@@ -10,7 +10,31 @@ const breakpoints = createBreakpoints({
 
 const theme = extendTheme({
   colors: {
-    black: "#16161D",
+    black: "#262626",
+    gray: {
+      "50": "#F2F2F2",
+      "100": "#DBDBDB",
+      "200": "#C4C4C4",
+      "300": "#ADADAD",
+      "400": "#969696",
+      "500": "#808080",
+      "600": "#666666",
+      "700": "#4D4D4D",
+      "800": "#333333",
+      "900": "#1A1A1A",
+    },
+    purple: {
+      "50": "#EAEBFB",
+      "100": "#C4C6F3",
+      "200": "#9EA1EB",
+      "300": "#787CE3",
+      "400": "#5258DB",
+      "500": "#2C33D3",
+      "600": "#2429A8",
+      "700": "#1B1F7E",
+      "800": "#121454",
+      "900": "#090A2A",
+    },
   },
   fonts: {
     heading: "Nanum Myeongjo",
