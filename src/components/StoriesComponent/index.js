8b8df57b083ac5ea7a3354fe8d73e421eb4c88fc
Peter Arnold <peter@entroprise.com>
2021-04-13 18:56:53 -0400

    asd

--- src/components/StoriesComponent/index.js
+++ src/components/StoriesComponent/index.js
@@ -14,52 +14,16 @@ function callback() {
   console.log("snapped");
 }
 
-class StoriesComponent extends React.Component {
-  container = React.createRef();
-
-  bindScrollSnap() {
-    const element = this.container.current;
-    const snapElement = new ScrollSnap(element, {
-      snapDestinationY: "90%",
-    });
-
-    snapElement.bind(callback);
-  }
-
-  componentDidMount() {
-    this.bindScrollSnap();
-  }
-
-  render() {
-    return (
-      <LayoutComponent>
-        <div
-          ref={this.container}
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            overflow: "auto",
-            position: "absolute",
-            top: 0,
-            // left: 0,
-            paddingTop: "5rem",
-            paddingLeft: " 1rem",
-            paddingRight: " 1rem",
-
-            width: "100%",
-            height: "100vh",
-          }}
-        >
-          {data.stories.map((story) => (
-            <CardComponent key={story.id}>
-              <StoryCard story={story} />
-            </CardComponent>
-          ))}
-        </div>
-      </LayoutComponent>
-    );
-  }
-}
+const StoriesComponent = () => {
+  return (
+    <LayoutComponent>
+      {data.stories.map((story) => (
+        <CardComponent key={story.id}>
+          <StoryCard story={story} />
+        </CardComponent>
+      ))}
+    </LayoutComponent>
+  );
+};
 
 export default StoriesComponent;
--- src/pages/stories.tsx
+++ src/pages/stories.tsx
@@ -1,28 +1,7 @@
-import { motion } from "framer-motion";
-import { Box } from "@chakra-ui/react";
-
 import StoriesComponent from "../components/StoriesComponent";
 
 const Stories = () => {
-  const MotionBox = motion(Box);
-
-  const transition = { duration: 0.5, ease: [0.43, 0.13, 0.23, 0.96] };
-
-  const pageVariants = {
-    initial: { scale: 0.9, opacity: 0 },
-    enter: { scale: 1, opacity: 1, transition },
-    exit: {
-      scale: 0.5,
-      opacity: 0,
-      transition: { ...transition, duration: 1.5 },
-    },
-  };
-
-  return (
-    <MotionBox variants={pageVariants}>
-      <StoriesComponent />
-    </MotionBox>
-  );
+  return <StoriesComponent />;
 };
 
 export default Stories;
