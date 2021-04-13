3fe27691922a700b0a0e44d99adbe1a98b33f1c7
Peter Arnold <parnold@retrieve.com>
2021-04-13 11:59:06 -0400

    glass

--- src/components/StoriesComponent/StoryLayout.js
+++ src/components/StoriesComponent/StoryLayout.js
@@ -3,13 +3,15 @@ import { Flex } from "@chakra-ui/react";
 export const StoryLayout = (props) => {
   return (
     <Flex
-      pt="3rem"
+      p="1rem"
+      pt="5rem"
       w="100%"
       h="100vh"
       direction="column"
       alignItems="center"
       justifyContent="flex-start"
-      bgGradient="linear(to-tr, teal.200, blue.500)"
+      // bgGradient="linear(to-tr, teal.200, blue.500)"
+      bgImage="url('https://uploads-ssl.webflow.com/5e2f7d4ac8eec2386bad1415/5e5c2eb163723e27a2ab4a09_WaterCourse.jpg')"
       {...props}
     />
   );
--- src/components/StoriesComponent/index.js
+++ src/components/StoriesComponent/index.js
@@ -1,9 +1,11 @@
 import { StoryCard } from "./StoryCard";
 import { StoryContainer } from "./StoryContainer";
 import { StoryLayout } from "./StoryLayout";
+import { GlassBackdrop } from "./GlassBackdrop";
 
 const StoriesComponent = () => (
   <StoryLayout>
+    <GlassBackdrop />
     <StoryContainer>
       <StoryCard />
     </StoryContainer>
