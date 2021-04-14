a6f77c76ffbdc2f8cad67ef295014a65b4857778
Peter Arnold <parnold@retrieve.com>
2021-04-14 11:09:13 -0400

    new layout

---
+++ src/components/LayoutComponent/AppLayout.tsx
@@ -0,0 +1,274 @@
+import {
+  Avatar,
+  Box,
+  BoxProps,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbProps,
+  Flex,
+  Heading,
+  HStack,
+  Input,
+  InputGroup,
+  InputGroupProps,
+  InputLeftElement,
+  InputProps,
+  Stack,
+  Text,
+  TextProps,
+  useBoolean,
+  useBreakpointValue,
+  useColorModeValue as mode,
+} from "@chakra-ui/react";
+import * as React from "react";
+import {
+  BsFillBookmarksFill,
+  BsFillInboxFill,
+  BsPencilSquare,
+  BsSearch,
+} from "react-icons/bs";
+import { HiMenu, HiChevronRight, HiX } from "react-icons/hi";
+import { data } from "./data";
+import { SidebarLink } from "./SidebarLink";
+
+type AppLayoutProps = {};
+
+export const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
+  const { isOpen, toggle } = useMobileMenuState();
+  return (
+    <Flex
+      height="100vh"
+      bg={mode("blue.800", "gray.800")}
+      overflow="hidden"
+      sx={{ "--sidebar-width": "256px" }}
+    >
+      <Box
+        as="nav"
+        display="block"
+        flex="1"
+        width="var(--sidebar-width)"
+        left="0"
+        py="5"
+        px="3"
+        color="gray.200"
+        position="fixed"
+      >
+        <Box fontSize="sm" lineHeight="tall">
+          <Box
+            as="a"
+            href="#"
+            p="3"
+            display="block"
+            transition="background 0.1s"
+            rounded="xl"
+            _hover={{ bg: "whiteAlpha.200" }}
+            whiteSpace="nowrap"
+          >
+            <HStack display="inline-flex">
+              <Avatar size="sm" name="Esther Collins" />
+              <Box lineHeight="1">
+                <Text fontWeight="semibold">Esther Collins</Text>
+                <Text
+                  fontSize="xs"
+                  mt="1"
+                  color={mode("whiteAlpha.700", "gray.400")}
+                >
+                  esther-colls@chakra.com
+                </Text>
+              </Box>
+            </HStack>
+          </Box>
+          <ScrollArea pt="5" pb="6">
+            <SidebarLink
+              display={{ base: "block", lg: "none" }}
+              mb="2"
+              icon={<BsSearch />}
+            >
+              Search
+            </SidebarLink>
+            <Stack pb="6">
+              <SidebarLink icon={<BsFillInboxFill />}>Inbox</SidebarLink>
+              <SidebarLink icon={<BsFillBookmarksFill />}>
+                Bookmarks
+              </SidebarLink>
+              <SidebarLink icon={<BsPencilSquare />}>Drafts</SidebarLink>
+            </Stack>
+            <Stack pb="6">
+              <NavSectionTitle>Chats</NavSectionTitle>
+              <SidebarLink>🎉 Inbox</SidebarLink>
+              <SidebarLink>👍 Personal</SidebarLink>
+              <SidebarLink>🦋 Work</SidebarLink>
+            </Stack>
+            <Stack>
+              <NavSectionTitle>Members</NavSectionTitle>
+              {data.users.map((user, index) => (
+                <SidebarLink
+                  key={index}
+                  avatar={
+                    <Avatar size="xs" name={user.name} src={user.image} />
+                  }
+                >
+                  {user.name}
+                </SidebarLink>
+              ))}
+            </Stack>
+          </ScrollArea>
+        </Box>
+      </Box>
+      <Box
+        flex="1"
+        p={{ base: "0", md: "6" }}
+        marginStart={{ md: "var(--sidebar-width)" }}
+        position="relative"
+        left={isOpen ? "var(--sidebar-width)" : "0"}
+        transition="left 0.2s"
+      >
+        <Box
+          maxW="2560px"
+          bg={mode("white", "gray.700")}
+          height="100%"
+          pb="6"
+          rounded={{ md: "lg" }}
+        >
+          <Flex direction="column" height="full">
+            <Flex
+              w="full"
+              py="4"
+              justify="space-between"
+              align="center"
+              px="10"
+            >
+              <Flex align="center" minH="8">
+                <MobileMenuButton onClick={toggle} isOpen={isOpen} />
+                <NavBreadcrumb />
+              </Flex>
+              <SearchInput />
+            </Flex>
+            <Flex direction="column" flex="1" overflow="auto" px="10" pt="8">
+              <Heading size="md" fontWeight="extrabold" mb="6">
+                Product Vision
+              </Heading>
+              <Box maxW="container.sm" flex="1">
+                {children}
+              </Box>
+            </Flex>
+          </Flex>
+        </Box>
+      </Box>
+    </Flex>
+  );
+};
+
+const MobileMenuButton = (props: { onClick: () => void; isOpen: boolean }) => {
+  const { onClick, isOpen } = props;
+  return (
+    <Box
+      display={{ base: "block", md: "none" }}
+      ml="-8"
+      mr="2"
+      as="button"
+      type="button"
+      rounded="md"
+      p="1"
+      fontSize="xl"
+      color="gray.500"
+      _hover={{ bg: "gray.100" }}
+      onClick={onClick}
+    >
+      <Box srOnly>{isOpen ? "Close Menu" : "Open Menu"}</Box>
+      {isOpen ? <HiX /> : <HiMenu />}
+    </Box>
+  );
+};
+
+const NavBreadcrumb = (props: BreadcrumbProps) => (
+  <Breadcrumb
+    fontSize="sm"
+    {...props}
+    separator={
+      <Box
+        as={HiChevronRight}
+        color="gray.400"
+        fontSize="md"
+        top="2px"
+        pos="relative"
+      />
+    }
+  >
+    <BreadcrumbItem color="inherit">
+      <BreadcrumbLink>Welcome</BreadcrumbLink>
+    </BreadcrumbItem>
+    <BreadcrumbItem color="inherit" isCurrentPage>
+      <BreadcrumbLink>Product Vision</BreadcrumbLink>
+    </BreadcrumbItem>
+  </Breadcrumb>
+);
+
+const SearchInput = (props: InputProps & { rootProps?: InputGroupProps }) => {
+  const { rootProps, ...rest } = props;
+  return (
+    <InputGroup
+      maxW="2xs"
+      size="sm"
+      variant="filled"
+      display={{ base: "none", lg: "block" }}
+      {...rootProps}
+    >
+      <InputLeftElement color="gray.400" pointerEvents="none">
+        <BsSearch />
+      </InputLeftElement>
+      <Input
+        {...rest}
+        placeholder="Search"
+        rounded="md"
+        _placeholder={{ color: "gray.400" }}
+      />
+    </InputGroup>
+  );
+};
+
+const ScrollArea = (props: BoxProps) => (
+  <Box
+    overflowY="auto"
+    height="80vh"
+    minH="px"
+    maxH="full"
+    {...props}
+    sx={{
+      "&::-webkit-scrollbar-track": {
+        bg: "transparent",
+      },
+      "&::-webkit-scrollbar": {
+        width: "4px",
+      },
+      "&::-webkit-scrollbar-thumb": {
+        bg: mode("blue.600", "gray.700"),
+        borderRadius: "20px",
+      },
+    }}
+  />
+);
+
+const useMobileMenuState = () => {
+  const [isOpen, actions] = useBoolean();
+  const isMobile = useBreakpointValue({ base: true, lg: false });
+  React.useEffect(() => {
+    if (isMobile == false) {
+      actions.off();
+    }
+  }, [isMobile, actions]);
+  return { isOpen, ...actions };
+};
+
+const NavSectionTitle = (props: TextProps) => (
+  <Text
+    casing="uppercase"
+    fontSize="xs"
+    fontWeight="semibold"
+    letterSpacing="wide"
+    paddingStart="3"
+    color="gray.400"
+    {...props}
+  />
+);
--- src/components/LayoutComponent/index.js
+++ src/components/LayoutComponent/index.js
@@ -1,16 +1,10 @@
 import { Container } from "@chakra-ui/react";
 import { DarkModeSwitch } from "../DarkModeSwitch";
 import RootBackGround from "./RootBackground";
+import { AppLayout } from "./AppLayout";
 
 const LayoutComponent = ({ children }) => {
-  return (
-    <RootBackGround>
-      <DarkModeSwitch />
-      <Container pt="5rem" pb="5rem">
-        {children}
-      </Container>
-    </RootBackGround>
-  );
+  return <AppLayout>{children}</AppLayout>;
 };
 
 export default LayoutComponent;
